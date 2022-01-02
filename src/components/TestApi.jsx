c2182890b252febc2de5894bd7ccece82daf6804
lucaslimouzin <limouzin.lucas@gmail.com>
2022-01-02 14:09:16 +0100

    upgrade page accueil

--- src/components/TestApi.jsx
+++ src/components/TestApi.jsx
@@ -1,48 +1,53 @@
-import React, { useState, useEffect,  } from 'react';
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
 
 const url = "https://api.unsplash.com/photos/random/?client_id=PNSdPNlri3siqZYnSDRjfdwtBX9f7a6aAYO_EubAxeQ"
-
-const TestApi = () => {
-    
-    
-    const [resoData, setResoData] = useState('Bonne année 2022 !!!'); //variable pour récupérer les résolutions (on initialise ici)
-    const [userData, setUserData] = useState(''); //variable pour récupérer le nom de l'auteur
-    const [userLinksData, setUserLinksData] = useState(''); //variable pour récupérer le lien de l'auteur
-    const [imgData, setImgData] = useState('http://www.adao.fr/wp-content/uploads/2014/12/fond-noir.png'); //variable pour récupérer l 'images
-    let user = '';
-    //pour récupérer les données dans une api via axios .data pour récupérer l'objet
-
-    const getImg = () => {
-        axios.get(url)
-        .then(result => {
-            setImgData(result.data.urls.regular) 
-            setUserData (result.data.user.first_name)
-            setUserLinksData (result.data.user.links.html)
-            
-        }).catch(err => {
-            console.log(err)
-        })
+export default class TestApi extends  React.Component {
+    state = {
+        resolutions:'',
     }
-    
-    // pour récupérer données des resolutions api créé
-    const getResolution = () => {
-        
+     
+        componentDidMount() {
+            this.fetchResolution();
+            this.fetchImage();
+        }
+
+    fetchResolution= () => {
         axios.get('https://retoolapi.dev/jlXeoU/data')
-        .then(result => {
-            const int = Math.floor(Math.random() * 150);
-            setResoData(result.data[int].resolutions ? result.data[int].resolutions :"la vie est belle" )
-        }).catch(err => {
-            console.log(err)
-        })
+      .then((response) => {
+        const int = Math.floor(Math.random() * 150);
+        this.setState({ resolutions: response.data[int].resolutions });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     }
-    
-    const execution = () => {
-        getImg();
-        getResolution();
-    }
-        
 
+    fetchImage= () => {
+        axios.get(url)
+      .then((response) => {
+        const { regular } = response.data.urls;
+        this.setState({ regular });
+        const { first_name } = response.data.user;
+        this.setState({ first_name });
+        const { html } = response.data.user.links.html;
+        this.setState({ html });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+    execution = () => {
+        this.fetchImage();
+        this.fetchResolution();
+    }
+
+    render() {
+
+        const imgData = this.state.regular;
+        const resoData = this.state.resolutions;
+        const userLinksData = this.state.html;
+        const userData = this.state.first_name;
         return (
             
             <div className="flex justify-start items-center flex-col h-screen bg-black">
@@ -54,16 +59,17 @@ const TestApi = () => {
                 <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 ">
                     <div className="w-4/5 h-1/3 flex items-center justify-center flex-col rounded-3xl shadow-[10px_10px] opacity-60 max-w-lg bg-white">
                         <p className="flex items-center justify-center text-center p-2 h-3/4 text-3xl">{resoData} </p> 
-                        <button onClick={execution} className="relative flex justify-center items-center rounded-full cursor-pointer border-2 border-red-700 w-auto h-1/5 p-2 ">Générer une résolution</button>
+                        <button onClick={this.execution} className="relative flex justify-center items-center rounded-full cursor-pointer border-2 border-red-700 w-auto h-1/5 p-2 ">Générer une résolution</button>
                     </div>
                     <div className="flex justify-end pt-3">
                         Photo by &nbsp; <a href={userLinksData} target="_blank" className="cursor-pointer font-bold">{userData}</a>&nbsp; in &nbsp; <a href="https://unsplash.com/" target="blank">Unsplash</a>
                     </div>
-                </div>
-                
-                
+                </div>     
             </div>
         )
     }
+}
 
-    export default TestApi
+
+
+
